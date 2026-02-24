9bb89ebc0dfad1694df5d15dfa31d269361db0cb
Stella Shin <sshin@lcs.on.ca>
2026-02-24 14:10:46 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by zimmer_h on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
