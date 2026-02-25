31b392c76fc976930db46fbdc38987b52189e08e
Stella Shin <sshin@lcs.on.ca>
2026-02-25 10:05:36 -0500

    Created helper view

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -11,30 +11,56 @@ struct WorldClockView: View {
     var body: some View {
         NavigationStack{
             VStack{
-                Text("World Clock")
+                //Ottawa
+                ExtractedView()
+                //Vancouber
+                ExtractedView()
             }
-                    .navigationTitle("World CLock")
-                    .toolbar {
-                        
-                        ToolbarItem(placement: .topBarLeading) {
-                            
-                            Button("Edit") {
-                                // Does nothing right now
-                            }
-                            
-                        }
-                        
-                        ToolbarItem(placement: .primaryAction){
-                            Button
-                            {
-                            }label:{
-                                Image(systemName: "plus")
-                            }
-                        }
+            .navigationTitle("World Clock")
+            .toolbar {
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    
+                    Button("Edit") {
+                        // Does nothing right now
                     }
+                    
+                }
+                
+                ToolbarItem(placement: .primaryAction){
+                    Button
+                    {
+                    }label:{
+                        Image(systemName: "plus")
+                    }
+                }
             }
         }
     }
+}
+
+struct ExtractedView: View {
+    
+    var body: some View {
+        HStack{
+            //left side
+            VStack{
+                Text ("Today, +0HRS")
+                Text ("Ottawa")
+                    .font(.system(.largeTitle, design: .default, weight: .thin))
+            }
+            
+            Spacer()
+            //Right side
+            Text("6:35")
+                .font(.system(size:64.0, weight: .thin, design: .default))
+            Text("AM")
+                .font (.system(.largeTitle, design: .default, weight: .thin))
+        }
+        
+    }
+        
+}
 
 
 #Preview {
